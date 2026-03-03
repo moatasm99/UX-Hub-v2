7e4a1e792a64fad4bf9fb7cc616fd94098b0f329
moatasm99 <moatasm99@gmail.com>
2026-03-03 13:22:18 +0200

    feat: add Vercel Analytics tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { AppProviders } from '@/app/providers'
 import { AppRouter } from '@/app/router'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
     <AppProviders>
       <AppRouter />
+      <Analytics />
     </AppProviders>
   )
 }
